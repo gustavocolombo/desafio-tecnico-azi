138cbb958411bb1b0cfff9a880f126d1aeb6c4aa
Gustavo Colombo <gusttavocolombo@gmail.com>
2025-01-19 20:45:49 -0300

    feat: initial structure of front-end vue

---
+++ front-todolist/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app theme="light">
+    <Header />
+
+    <v-main>
+      <h1>Hello world</h1>
+    </v-main>
+  </v-app>
+</template>
---
+++ front-todolist/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app-bar
+    flat
+    class="border-b align-items-center d-flex"
+    title="Todo-list AZ Tecnologia em Gestão"
+  >
+    <v-spacer></v-spacer>
+    <v-text-field
+      class="mt-5 pa-10"
+      label="Digite o id da tarefa"
+      clearable
+      variant="solo"
+      append-icon="mdi-magnify"
+    />
+  </v-app-bar>
+</template>
